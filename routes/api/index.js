2b44904fd81a762945ed75c82823024e62fddc65
Mitchell Wood <mitchellwood11@gmail.com>
2023-03-04 13:44:59 -0600

    added routing

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,1 +1,8 @@
-'filler'
+const router = require("express").Router();
+const userRoutes = require("./user-routes");
+const thoughtRoutes = require("./thought-routes");
+
+router.use("/users", userRoutes);
+router.use("/thoughts", thoughtRoutes);
+
+module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -1,1 +1,10 @@
-'filler'
+const router = require("express").Router();
+const apiRoutes = require("./api");
+
+router.use("/api", apiRoutes);
+
+router.use((req, res) => {
+  res.status(404).send("<h1>Wrong Route!</h1>");
+});
+
+module.exports = router;
